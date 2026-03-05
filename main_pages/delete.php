c4662daa76da1e868cd1eb4e926eb24f5a0edffa
ominya-badr <ominya.abdalhameed.23016@gmail.com>
2026-03-05 15:22:49 +0200

    Delete main_pages directory

--- main_pages/delete.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include '../includes/db.php';
-if(isset($_GET['id'])) {
-    $stmt =$pdo->prepare('DELETE FROM teams WHERE id = ?');
-    $stmt->execute([$_GET['id']]);
-    header('location: teams.php');
-}
-?>
--- main_pages/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../includes/db.php';
-include '../includes/header.php';
-
-
-?>
-<?php
-include '../includes/footer.php';
-?>
